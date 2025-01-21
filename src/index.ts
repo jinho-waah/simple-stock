4c6a9e818a55b4fdb35ae8a2362b3d8560fb89ee
jinho-waah <andrewjo950515@gmail.com>
2025-01-21 14:13:00 +0900

    refactor

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+// src/index.ts
+
+/*
+ * Components
+ */
+
+// export { default as OHLCChart } from "./components/OHLCChart";
+// export { default as VChart } from "./components/VChart";
+// export { default as ChartCanvas } from "./components/ChartCanvas";
+// export { default as Tooltip } from "./components/Tooltip";
+
+/*
+ * Hooks
+ */
+// export { default as useStockData } from "./hooks/useStockData";
+
+/*
+ * Utilities
+ */
+// export * from "./utils/formatData"; // 여러 유틸리티를 한꺼번에 export
+
+/*
+ * Types
+ */
+// export type { StockData, OHLCVData } from "./types/StockData";
