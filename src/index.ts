90feaafd72af58e8c3d8752dfd3712bd934787ad
jinho-waah <andrewjo950515@gmail.com>
2025-02-09 08:32:17 +0900

    refactor: modify index.ts

--- src/index.ts
+++ src/index.ts
@@ -8,16 +8,11 @@
 // export { default as VChart } from "./components/VChart";
 // export { default as ChartCanvas } from "./components/ChartCanvas";
 // export { default as Tooltip } from "./components/Tooltip";
-
+export { default as OHLCVChart } from "./components/OHLCVChart";
 /*
  * Hooks
  */
 // export { default as useStockData } from "./hooks/useStockData";
-
-/*
- * Utilities
- */
-// export * from "./utils/formatData"; // 여러 유틸리티를 한꺼번에 export
 
 /*
  * Types
