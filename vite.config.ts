b57b857d4611b02677c16cec3c403fe85b111e06
jinho-waah <andrewjo950515@gmail.com>
2025-02-10 12:50:31 +0900

    refactor: delete /src , create /lib

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: resolve(__dirname, "lib/index.ts"), // lib 디렉토리 사용
+      name: "simple-stock",
+      fileName: (format) => `simple-stock.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
