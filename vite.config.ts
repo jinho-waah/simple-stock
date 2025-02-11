272004c626240d3a0c2fb3c6e759f48d4ede12bf
jinho-waah <andrewjo950515@gmail.com>
2025-02-11 19:57:02 +0900

    docs: change gen

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts"; 
 
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, "lib/index.ts"), // lib 디렉토리 사용
+      entry: resolve(__dirname, "lib/index.ts"),
       name: "simple-stock",
       fileName: (format) => `simple-stock.${format}.js`,
     },
@@ -18,4 +19,5 @@ export default defineConfig({
       },
     },
   },
+  plugins: [dts()],
 });
